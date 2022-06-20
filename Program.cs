2b5fbc27019c1b1f2a5d05c8f4c39bdce384d287
bruno0011 <brunosouzviana@hotmail.com>
2022-06-20 04:18:42 -0300

    projeto final

--- Program.cs
+++ Program.cs
@@ -1,133 +1,186 @@
 ﻿using System;
 
-namespace Calculadora2022;
-
+namespace exercicios_etec
+{
 class Program
 {
 static void Main(string[] args)
 {
-Console.WriteLine("Olá, meu nome é Duny! Hoje irei te ajudar em seus calculos.");
-Console.WriteLine("Antes de começarmos, gostaria de saber seu nome");
-Console.Write("Digite seu nome: ");
-string nome=Console.ReadLine();
-            
-Console.Clear();
-Console.Write("Prazer em conhece-lo " + nome + "\nPressione qualquer tecla para continuar");
-Console.ReadKey();
-Console.Clear();
-            
-Console.WriteLine(nome + " eu tenho uma lista de seis operações matemáticas que são elas:Soma;\nSubtração;\nMultiplicação;\nDivisão;\nRaiz Quadrada;\nPotência");
-Console.Write("Escolha uma das seis e então poderemos começar a calcular!\nPressione qualquer tecla para continuar");
-Console.ReadKey();
-Console.Clear();
+string opcao;
 
-            
-string sair;
 do
 {
-                
-Console.Write(nome + " Digite uma das operações matemáticas da lista: ");
-string operação=Console.ReadLine().ToLower();
-                
-if(operação=="soma")
-Program.CalcularSoma();          
-                
-else if(operação=="subtração")
-Program.CalcularSubtração();
-                
-else if(operação=="multiplicação")
-Program.CalcularMultiplicação();
-                
-else if(operação=="divisão")
-Program.CalcularDivisão();
-                
-else if(operação=="raiz quadrada")
-Program.CalcularRaizQuadrada();
-                
-else if(operação=="potência")
-Program.CalcularPotênciação();
-                
-else
-Console.WriteLine(nome + " você não digitou nenhuma das operações da lista ou deve ter digitado errado");
+exibeMenu();
+opcao = Console.ReadLine().Trim().ToUpper();
 
-Console.Write("Deseja parar de calcular, SIM ou NÃO: ");
-sair=Console.ReadLine().ToUpper();
-Console.Clear();
-
-            
-} while (sair != "SIM" && sair != "S");
-
-Console.Write("Deseja ver os créditos do programa?: ");
-string resposta=Console.ReadLine().ToLower();
-
-if(resposta == "sim" || resposta == "s")
-Console.WriteLine("Prazer " +nome+ " meu nome é Bruno e eu sou o criador deste programa.\nFiz este programa com a intenção de ajudar pessoas como você\na resolver contas simples de matemática.");
-
-else
-Console.WriteLine("OK, então pressione qualquer tecla para continuar");
+switch (opcao)
+                {
+case "A":
+Console.WriteLine("Sou o programa");
+break;
+case "S":
+calculaSoma();
+break;
+case "B":
+calculaSubtracao();
+break;
+case "M":
+calculaProduto();
+break;
+case "D":
+calculaDivisao();
+break;
+case "E":
+calculaExponenciacao();
+break;
+case "R":
+calculaRadiciacao();
+break;
+case "X":
+Console.WriteLine("Obrigado por utilizar!");
+break;
+default:
+Console.WriteLine("Funcionalidade indisponível");
+break;
+                }
+Console.WriteLine("Pressione qualquer tecla para continuar...\n");
 Console.ReadKey();
-
 Console.Clear();
-Console.WriteLine("Obrigrado " + nome + " por usar a calculadora!\nAté a próxima!\nPressione qualquer tecla para finalizar");
-Console.ReadKey();
-            
 }
-
-static void CalcularSoma()
-{
-Console.WriteLine("Digite dois números");
-Console.Write("1° número: ");
-double valor1=Convert.ToDouble(Console.ReadLine());
-Console.Write("2° número: ");
-double valor2=Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"{valor1} + {valor2} = " + (valor1+valor2));
+while (opcao != "X");
 
 }
 
-static void CalcularSubtração()
+static void exibeMenu()
 {
-Console.WriteLine("Digite dois números");
-Console.Write("1° número: ");
-double valor3=Convert.ToDouble(Console.ReadLine());
-Console.Write("2° número: ");
-double valor4=Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"{valor3} - {valor4} = " + (valor3-valor4)); 
+Console.WriteLine("============================");
+Console.WriteLine("=           Menu           =");
+Console.WriteLine("= S - Soma (+)             =");
+Console.WriteLine("= B - Subtração (-)        =");
+Console.WriteLine("= M - Multiplicação (x)    =");
+Console.WriteLine("= D - Divisão (/)          =");
+Console.WriteLine("= E - Exponenciação (¹²³)  =");
+Console.WriteLine("= R - Radiciação (V)       =");
+Console.WriteLine("============================");
+Console.WriteLine("= A - Sobre o programa     =");
+Console.WriteLine("= X - Sair                 =");
+Console.WriteLine("============================");
 }
 
-static void CalcularMultiplicação()
+        static void calculaSoma()
 {
-Console.WriteLine("Digite dois números");
-Console.Write("1° número: ");
-double valor5=Convert.ToDouble(Console.ReadLine());
-Console.Write("2° número: ");
-double valor6=Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"{valor5} X {valor6} = " + (valor5*valor6));
+double numeroUm, numeroDois, resultado;
+Console.WriteLine("============================");
+Console.WriteLine("=          Soma            =");
+Console.WriteLine("============================");
+
+numeroUm = digitaNumeroOrdenado("primeiro");
+numeroDois = digitaNumeroOrdenado("segundo");
+
+resultado = numeroUm + numeroDois;
+
+Console.WriteLine($"A soma de {numeroUm} + {numeroDois} é igual a {resultado:N2}");
+        }
+
+static void calculaSubtracao()
+{
+double numeroUm, numeroDois, resultado;
+Console.WriteLine("============================");
+Console.WriteLine("=         Subtração        =");
+Console.WriteLine("============================");
+
+numeroUm = digitaNumeroOrdenado("primeiro");
+numeroDois = digitaNumeroOrdenado("segundo");
+
+resultado = numeroUm - numeroDois;
+
+Console.WriteLine($"A subtração de {numeroUm} - {numeroDois} é igual a {resultado}");
 }
 
-static void CalcularDivisão()
+static void calculaProduto()
 {
-Console.WriteLine("Digite dois números");
-Console.Write("1° número: ");
-double valor7=Convert.ToDouble(Console.ReadLine());
-Console.Write("2° número: ");
-double valor8=Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"{valor7} / {valor8} = " + (valor7/valor8));
+double numeroUm, numeroDois, resultado;
+Console.WriteLine("============================");
+Console.WriteLine("=       Multiplicação      =");
+Console.WriteLine("============================");
+
+numeroUm = digitaNumeroOrdenado("primeiro");
+
+= digitaNumeroOrdenado("segundo");
+
+resultado = numeroUm * numeroDois;
+
+Console.WriteLine($"O produto de {numeroUm} x {numeroDois} é igual a {resultado:N2}");
+        }
+
+static void calculaDivisao()
+        {
+double numeroUm, numeroDois, resultado;
+Console.WriteLine("============================");
+Console.WriteLine("=          Divisão         =");
+Console.WriteLine("============================");
+
+numeroUm = digitaNumeroOrdenado("primeiro");
+numeroDois = digitaNumeroOrdenado("segundo");
+
+if (numeroDois == 0)
+{
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Não é possível a divisão por zero!");
+Console.ResetColor();
+            }
+else
+            {
+resultado = numeroUm / numeroDois;
+Console.WriteLine($"A divisão de {numeroUm} / {numeroDois} é igual a {resultado:N2}");
+}
 }
 
-static void CalcularRaizQuadrada()
+static void calculaExponenciacao()
 {
-Console.WriteLine("Digite um número");
-Console.Write("1° número: ");
-double valor9=Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"A raiz quadrada de {valor9} é = " + (Math.Sqrt(valor9)));
- }
+double @base, potencia, resultado;
+Console.WriteLine("============================");
+Console.WriteLine("=       Exponenciação      =");
+Console.WriteLine("============================");
+@base = digitaNumero("Digite a base: ");
+potencia = digitaNumero("Digite a potência: ");
 
-static void CalcularPotênciação()
+
+Console.WriteLine($"A potência de {@base} elevado a {potencia} é igual a {resultado:N2}");
+}
+
+        static void calculaRadiciacao()
 {
-Console.WriteLine("Digite dois números");
-Console.Write("1° número: ");double valor10=Convert.ToDouble(Console.ReadLine());
-Console.Write("2° número: ");double valor11=Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"{valor10} elevado a {valor11} é = " + (Math.Pow(valor10, valor11)));
+double numero, resultado;
+Console.WriteLine("============================");
+Console.WriteLine("=         Radiciação       =");
+Console.WriteLine("============================");
+
+numero = digitaNumero("Digite um número: ");
+
+            if (numero < 0)
+{
+Console.WriteLine("Não existem raízes de números negativos!");
+}
+else
+{
+resultado = Math.Sqrt(numero);
+Console.WriteLine($"A raiz de {numero} é igual a {resultado:N2}");
+}
+}
+
+static double digitaNumero(string mensagem)
+        {
+double numero = 0;
+Console.Write(mensagem);
+Double.TryParse(Console.ReadLine(), out numero);
+return numero;
+}
+
+static double digitaNumeroOrdenado(string ordem)
+{
+            return digitaNumero($"Digite o {ordem} número");
+}
 
 }
 }
